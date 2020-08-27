52853c72094ad5543935d58fd67ebf067e4c57eb
kuk6467 <kuk6467@nate.com>
2020-08-27 17:43:18 +0900

    dd

--- board/settings.py
+++ board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-$g9qk0cwy#*a43u%i4z)sa1ifj0253*ucns7mwj78b*-*3e9f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.pythonanywhere.com']
+ALLOWED_HOSTS = [os.path.join(BASE_DIR, 'db.sqlite3''.pythonanywhere.com')]
 
 
 # Application definition
